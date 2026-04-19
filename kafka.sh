bf14ad5b1583d8e9b1b92486424ad1c95d4f78b5
konstantin <konstantinkhutorenko@icloud.com>
2026-04-19 16:59:40 +0300

    поменял скрипт развертывания

--- kafka.sh
+++ kafka.sh
@@ -1,38 +1,21 @@
-#sh
+#!/bin/sh
 
-docker exec -it kafka_task bash
+docker exec kafka_task /opt/bitnami/kafka/bin/kafka-topics.sh \
+  --bootstrap-server localhost:9092 \
+  --create --topic tasks.created --partitions 3 --replication-factor 1
 
-kafka-topics.sh \
---bootstrap-server localhost:9092 \
---create \
---topic tasks.created \
---partitions 3 \
---replication-factor 1
+docker exec kafka_task /opt/bitnami/kafka/bin/kafka-topics.sh \
+  --bootstrap-server localhost:9092 \
+  --create --topic tasks.retry.1m --partitions 3 --replication-factor 1
 
-kafka-topics.sh \
---bootstrap-server localhost:9092 \
---create \
---topic tasks.retry.1m \
---partitions 3 \
---replication-factor 1
+docker exec kafka_task /opt/bitnami/kafka/bin/kafka-topics.sh \
+  --bootstrap-server localhost:9092 \
+  --create --topic tasks.retry.5m --partitions 3 --replication-factor 1
 
-kafka-topics.sh \
---bootstrap-server localhost:9092 \
---create \
---topic tasks.retry.5m \
---partitions 3 \
---replication-factor 1
+docker exec kafka_task /opt/bitnami/kafka/bin/kafka-topics.sh \
+  --bootstrap-server localhost:9092 \
+  --create --topic tasks.retry.30m --partitions 3 --replication-factor 1
 
-kafka-topics.sh \
---bootstrap-server localhost:9092 \
---create \
---topic tasks.retry.30m \
---partitions 3 \
---replication-factor 1
-
-kafka-topics.sh \
---bootstrap-server localhost:9092 \
---create \
---topic tasks.dlq \
---partitions 3 \
---replication-factor 1
+docker exec kafka_task /opt/bitnami/kafka/bin/kafka-topics.sh \
+  --bootstrap-server localhost:9092 \
+  --create --topic tasks.dlq --partitions 3 --replication-factor 1
---
+++ makefile
@@ -0,0 +1,28 @@
+.PHONY: up down logs clean migrate kafka-create-topics infra-up infra-down infra-logs
+
+up:
+	docker-compose up -d
+
+down:
+	docker-compose down
+
+logs:
+	docker-compose logs -f
+
+logs-%s:
+	docker-compose logs -f $*
+
+clean:
+	docker-compose down -v
+
+migrate:
+	docker-compose run --rm migrate
+
+kafka-create-topics:
+	bash kafka.sh
+
+infra-up:
+	docker-compose up -d postgres migrate kafka kafka-ui
+
+infra-down:
+	docker-compose down postgres migrate kafka kafka-ui -v
